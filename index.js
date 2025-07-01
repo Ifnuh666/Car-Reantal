96f6ec5369cc1ff3979c39a8f10488595e6c3b48
Ifnuh666 <damir.gaznul@gmail.com>
2025-07-01 23:55:26 +0300

    Добавление JS-кода и небольшая стилизация страниц

---
+++ index.js
@@ -0,0 +1,65 @@
+function toggleButton() {
+  var checkbox = document.getElementById("myCheckbox");
+  var button = document.getElementById("myButton");
+
+  if (checkbox.checked) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function toggleButton1() {
+  var checkbox = document.getElementById("myCheckbox1");
+  var button = document.getElementById("myButton1");
+
+  if (checkbox.checked) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function toggleButton2() {
+  var checkbox = document.getElementById("myCheckbox2");
+  var button = document.getElementById("myButton2");
+
+  if (checkbox.checked) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function toggleButton3() {
+  var checkbox = document.getElementById("myCheckbox3");
+  var button = document.getElementById("myButton3");
+
+  if (checkbox.checked) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function toggleButton4() {
+  var checkbox = document.getElementById("myCheckbox4");
+  var button = document.getElementById("myButton4");
+
+  if (checkbox.checked) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function toggleButton5() {
+  var checkbox = document.getElementById("myCheckbox5");
+  var button = document.getElementById("myButton5");
+
+  if (checkbox.checked) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
